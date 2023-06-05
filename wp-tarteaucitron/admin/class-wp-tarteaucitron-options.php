d645bb3e9914bc9c5ebc167761db88106a57c7f3
William Mead <william.mead@manchenumerique.fr>
2023-06-05 15:52:04 +0200

    Fixed plugin_action_links filter

--- wp-tarteaucitron/admin/class-wp-tarteaucitron-options.php
+++ wp-tarteaucitron/admin/class-wp-tarteaucitron-options.php
@@ -13,7 +13,7 @@ class WP_tateaucitron_Options {
 	public function setup(): void {
 		add_action( 'admin_menu', array($this, 'setup_submenu_with_page' ) );
 		add_action( 'admin_init', array(&$this, 'setup_settings' ) );
-		add_filter( 'plugin_action_links_' . plugin_basename(WP_TARTEAUCITRON_PLUGIN_FILE_PATH), 'plugin_settings_link' );
+		add_filter( 'plugin_action_links_' . plugin_basename(WP_TARTEAUCITRON_PLUGIN_FILE_PATH), array( $this, 'plugin_settings_link') );
 	}
 
 	/**
