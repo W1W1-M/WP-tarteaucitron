8812aafb25e970cde771be187e3adc375b0ac6e5
William Mead <william.mead@manchenumerique.fr>
2024-01-24 11:00:54 +0100

    Added free software header

--- wp-tarteaucitron/uninstall.php
+++ wp-tarteaucitron/uninstall.php
@@ -1,4 +1,17 @@
 <?php /** @noinspection PhpRedundantClosingTagInspection */
+declare( strict_types=1 );
+
+/*
+ * This file is part of WP-tarteaucitron.
+ *
+ * WP-tarteaucitron is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * WP-tarteaucitron is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 wp_tarteaucitron_uninstall();
 
